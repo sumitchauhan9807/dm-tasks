615fc7b5000807d02ba001d628514cf6fb7f3f36
FarisStGermain <153274889+FarisStGermain@users.noreply.github.com>
2024-05-09 15:56:38 +0200

    Add files via upload

--- dm-task-1/src/App.js
+++ dm-task-1/src/App.js
@@ -5,16 +5,79 @@
 // userNameEmail= "flirttool",password="12345678"
 // You can use axios or inbuild fetch API
 
+import React, { useState, useEffect } from 'react';
+
 function App() { 
+
+const [userNameEmail, setName] = useState("")
+const [password, setPassword] = useState("")
+const [ran, setRan] = useState(0)
+const [fullName, setFullName] = useState ("")
+const [email, setEmail] = useState ("")
+const [errorMsg, setError] = useState ("")
+
+
+
+const handleSubmit = (e) => {
+  if (!e) return 
+
+  e.preventDefault()
+  let userName = ""
+  let passWord = ""
+
+    if(e){
+    userName = e.target.email.value
+    passWord = e.target.pwd.value
+    } 
+
+    setName(userName)
+    setPassword(passWord)
+  }
+  
+
+  if(userNameEmail != "" && password != "" && ran === 0){
+      {fetch("https://mazutech.online/api/user/login",{
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userNameEmail, password })
+        }).then(response => {
+          if (response.ok) {return response.json()
+      } else {
+      console.log(response)
+      setError("Error code " + response.status) 
+      setFullName("")
+      setEmail("")
+      throw new Error
+        }
+      }
+        
+    ).then(data => {
+        if(data.user.full_name != "" && data.user.last_name != "")
+          {
+            setFullName(data.user.first_name + data.user.last_name)
+            setRan(1);
+            setEmail(data.user.email)
+            setError("")
+            console.log(data.message)
+          }else {
+            
+          }
+      }).catch((error) => {
+        console.log(error)
+      })
+      }
+    }
+    
+
   return (
-    <div classname="App">
+    <div className="App">
         <div className="container" style={{background:"lightblue",padding:"20px",marginTop:"20px"}}>
           <br/>
           <div className="card">
             <div className="card-body"><center>Login Form</center></div>
           </div>
           <br/>
-          <form action="">
+          <form onSubmit ={handleSubmit}>
               <div className="form-group">
                 <label htmlFor="email">Email address:</label>
                 <input type="text" className="form-control" placeholder="Enter email" id="email" />
@@ -24,8 +87,12 @@ function App() {
                 <input type="password" className="form-control" placeholder="Enter password" id="pwd" />
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            </form>  
+            <p>{fullName}</p>
+            <p>{email}</p>
+            <h1 style={{color: "red"}}>{errorMsg}</h1>
         </div>
+        
       </div>
   );
 }
